117f093d361d3d203c3572385f775184c0776752
nishanth4894 <nishanth4894@gmail.com>
2023-03-27 22:49:16 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income",
+      version="0.0.1",
+      author="nishanth",
+      author_email="nishanth4894@gmsil.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask","scikit-learn"]
+      )
+
+
